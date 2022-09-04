dabe9173f5868894e4debe942d4dea951ac9a60d
Rishitha Dilshan <87588442+IT19967080@users.noreply.github.com>
2022-09-04 12:28:55 +0530

    initialize complaint components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import  { React} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Addcomplaint from "./Components/complaintManagement/addcomplaint";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App() {
+  return(
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/addcomplaint" element = {<Addcomplaint/>} exact/>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
-
-export default App;
